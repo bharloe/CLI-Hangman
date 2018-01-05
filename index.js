5247d1896121885d1a1fc3e0c9c24441f611d189
Ben Harloe <ben.harloe@gmail.com>
2018-01-04 17:17:35 -0800

    wrote movies js to select random movie

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-var inquirer = require(inquirer);
+var inquirer = require("inquirer");
+var Movies = require("./js/movies");
+
+var movieGen = new Movies();
+var movie = movieGen.randomMovie();
+var hangman = hangmanGen(movie, letter);
+
+play();
+
+function play() {
+  console.log(movie);
+  ;
+
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "letter",
+        message: "Guess a letter!\n"+hangman
+      }
+    ])
+    .then(function(response) {
+      if (movie.includes(response.letter)) {
+        console.log("letter is in the word");
+      } else {
+        console.log("Wrong :(");
+        play();
+      }
 
 
-inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Who are you???"
-    }
-]).then(function(user) {
-    // If the user guesses the password...
-    if (user.myPassword === "myHouse") {
-      console.log("==============================================");
-      console.log("==============================================");
-    }
-    // If the user doesn't guess the password...
-    else {
-      console.log("==============================================");
-      console.log("==============================================");
-    }
-  });
+      
+    });
+}
+
+function hangmanGen(movie, letter) {
+  var gameString = movie;
+  if (gameString[i] === letter) {
+    
+  }
+
+}
+
---
+++ js/movies.js
@@ -0,0 +1,19 @@
+function MovieGen() {
+    this.moviesArray = [
+        "Interstellar",
+        "Cloud Atlas",
+        "2001: A Space Odyssey",
+        "Blade Runner 2049"
+    ];
+
+    this.randomMovie = function() {
+        var movie = this.moviesArray[Math.floor(Math.random() * this.moviesArray.length)];
+        var index = this.moviesArray.indexOf(movie)
+        if (index > -1) {
+            this.moviesArray.splice(index, 1);
+        };
+        return movie;
+    };
+}
+
+module.exports = MovieGen;
